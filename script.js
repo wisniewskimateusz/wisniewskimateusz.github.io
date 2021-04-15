048c8817186cbac101ef13be45a784f06c9ccc6c
Mateusz <mateusz@com>
2021-04-15 19:58:00 +0200

    apiRoot fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'http://localhost:8080/v1/task/';
-   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+   const apiRoot = 'https://frozen-lowlands-32052.herokuapp.com/v1/task/';
+   const trelloApiRoot = 'https://frozen-lowlands-32052.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
