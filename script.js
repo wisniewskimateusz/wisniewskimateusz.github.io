587f8d07eb091178e1a74b5209dd90d0957d1166
Mateusz <mateusz@com>
2021-04-19 22:28:53 +0200

    new bacend url

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://frozen-lowlands-32052.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://frozen-lowlands-32052.herokuapp.com/v1/trello/';
+   const apiRoot = 'https://shrouded-harbor-77341.herokuapp.com/v1/task/';
+   const trelloApiRoot = 'https://shrouded-harbor-77341.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
