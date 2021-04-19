da64023d58079dbc911771de25efb33103770b3a
Mateusz <mateusz@com>
2021-04-19 22:45:14 +0200

    new backend url

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://shrouded-harbor-77341.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://shrouded-harbor-77341.herokuapp.com/v1/trello/';
+   const apiRoot = 'https://mighty-taiga-91271.herokuapp.com/v1/task/';
+   const trelloApiRoot = 'https://mighty-taiga-91271.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
